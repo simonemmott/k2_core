884e8f9c4ae9d3ef921a9c2260da33449233384b
simon <simon.emmott@yahoo.co.uk>
2019-06-29 01:37:58 +0100

    Added config_class_name to DomainMixin

--- k2_core/mixins/domain.py
+++ k2_core/mixins/domain.py
@@ -1,7 +1,13 @@
 from . import register
+import k2_util
 
 class DomainMixin(object):
     
     @register('k2_domain.models.domain', 'Domain')
     def __str__(self):
         return self.title
+    
+    def config_class_name(self):
+        return '{name}Config'.format(name=k2_util.to_class_case(self.title))
+    
+    
